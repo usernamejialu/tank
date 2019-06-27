06deeb8eda0bfa928b8ff0ec6ecc794b4839ce3a
Tommy <woo.tommy@icloud.com>
2019-06-27 12:48:50 +0800

    update ./Audio

--- Scripts/Audio/UIButtonAudio.cs
+++ Scripts/Audio/UIButtonAudio.cs
@@ -1,21 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 namespace Tanks.Audio
 {
-	// This class is intended to be attached to all in-game buttons. It will call the UIAudioManager to play either the default button sound, or an override sound specified in inspector.
-	public class UIButtonAudio : MonoBehaviour 
-	{
-		[SerializeField]
-		protected AudioClip m_OverrideClip;
+  // Call the UIAudioManager to play the default button sound
+  public class UIButtonAudio : MonoBehaviour
+  {
+    [SerializeField]
+    protected AudioClip m_OverrideClip;
 
-		//Assign this to the OnClick event of the button it's attached to.
-		public void OnClick()
-		{
-			if(UIAudioManager.s_InstanceExists)
-			{
-				UIAudioManager.s_Instance.PlayButtonEffect(m_OverrideClip);
-			}
-		}
-	}
+    public void OnClick()
+    {
+      if (UIAudioManager.s_InstanceExists)
+      {
+        UIAudioManager.s_Instance.PlayButtonEffect(m_OverrideClip);
+      }
+    }
+  }
 }
